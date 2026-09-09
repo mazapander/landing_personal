561db2ff32528368de828557a8fd4b883fee7ee7
Ander Fernandez <98283345+mazapander@users.noreply.github.com>
2026-09-09 07:30:24 +0200

    feat: publish automation urls in sitemap

--- frontend/src/pages/sitemap.xml.ts
+++ frontend/src/pages/sitemap.xml.ts
@@ -3,7 +3,8 @@ import { getCollection } from 'astro:content'
 import { sitemapUrls } from '@/lib/sitemap-urls.mjs'
 
 export const GET: APIRoute = async () => {
-  const urls = sitemapUrls(await getCollection('projects'))
+  const [projects, automations] = await Promise.all([getCollection('projects'), getCollection('automations')])
+  const urls = sitemapUrls(projects, automations)
   const body = `<?xml version="1.0" encoding="UTF-8"?>\n<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">${urls.map((url) => `\n  <url><loc>${url}</loc></url>`).join('')}\n</urlset>`
 
   return new Response(body, { headers: { 'Content-Type': 'application/xml; charset=utf-8' } })
